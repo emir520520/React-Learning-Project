3b1ee6da258b61abf24cd0691598cfe53010e186
emir520520 <frankfang111@gmail.com>
2022-12-28 23:47:48 -0500

    Dec 28, 2022

--- 002_-_animals/src/App.js
+++ 002_-_animals/src/App.js
@@ -1,5 +1,17 @@
+import {useState} from "react";
+
 function App() {
-  return <div>Show animal list here!</div>;
+    const [count, setCount]=useState(0);
+    const handleClick=()=>{
+        setCount(count+1);
+    };
+
+  return (
+      <div>
+        <button onClick={handleClick}>Add animal</button>
+          <div>Number of animals: {count}</div>
+      </div>
+  );
 }
 
 export default App;
