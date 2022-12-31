7ce3d3c14d106cb2bf1c0ca241059ecf77152318
emir520520 <frankfang111@gmail.com>
2022-12-31 10:11:36 -0500

    Dec 31, 2022
    Events, State, lists display

--- 002_-_animals/src/AnimalShow.js
+++ 002_-_animals/src/AnimalShow.js
@@ -1,5 +1,32 @@
-function AnimalShow() {
-  return <div>Cow!</div>;
+import {useState} from "react";
+
+import './css/AnimalShow.css';
+
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+
+const imgMap={
+  bird, cat, cow, dog, gator, horse
+};
+
+function AnimalShow({type}) {
+  const [clicks, addClick]=useState(0);
+
+  const handleClicks=()=>{
+    addClick(clicks+1);
+  };
+
+  return (
+      <div className="item" onClick={handleClicks}>
+        <img className="img-animal" alt="animal" src={imgMap[type]} style={{width: '250px', height: '200px'}} />
+        <img className="img-heart" alt="heart" src={heart} style={{width: 25+10*clicks+'px', height: 20+10*clicks+'px'}} />
+      </div>
+  );
 }
 
 export default AnimalShow;
--- 002_-_animals/src/App.js
+++ 002_-_animals/src/App.js
@@ -1,15 +1,27 @@
 import {useState} from "react";
+import AnimalShow from './AnimalShow';
+import './css/App.css';
+
+function getRandomAnimal(){
+    const animals=['bird','cat','cow','dog','gator','horse'];
+
+    return animals[Math.floor(Math.random()*animals.length)];
+}
 
 function App() {
-    const [count, setCount]=useState(0);
+    const [animals, setAnimal]=useState([]);
     const handleClick=()=>{
-        setCount(count+1);
+        setAnimal([...animals, getRandomAnimal()]);
     };
 
+    const renderedAnimals=animals.map((animalType, index)=>{
+        return (<AnimalShow type={animalType} key={index} />);
+    });
+
   return (
-      <div>
+      <div className="app">
         <button onClick={handleClick}>Add animal</button>
-          <div>Number of animals: {count}</div>
+          <div className="list">{renderedAnimals}</div>
       </div>
   );
 }
